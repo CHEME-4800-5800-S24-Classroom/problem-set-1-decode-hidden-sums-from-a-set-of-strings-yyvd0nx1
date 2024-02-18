5ac65ff251519cee5f24405a9bc1350b19abc34a
yyvd0nx1 <dg567@cornell.edu>
2024-02-18 16:11:52 -0500

    draft02182024

--- src/Factory.jl
+++ src/Factory.jl
@@ -2,7 +2,11 @@
 # ===== PRIVATE METHODS BELOW HERE =================================================================================== #
 # TODO: Put private helper methods here. Don't forget to use the _ naming convention, and to add basic documentation.
 # ===== PRIVATE METHODS ABOVE HERE =================================================================================== #
-
+function _build(record::String)
+    puzzle = MyPuzzleRecordModel()
+    puzzle.len = length(record)
+    #puzzle.characters = 
+end
 # ===== PUBLIC METHODS BELOW HERE =================================================================================== #
 # TODO: Make a build method to construct MyPuzzleRecordModel types
 # ===== PUBLIC METHODS ABOVE HERE =================================================================================== #
--- src/Types.jl
+++ src/Types.jl
@@ -5,4 +5,9 @@ TODO: Add documentation
 """
 # TODO: Finish the MyPuzzleRecordModel type
 mutable struct MyPuzzleRecordModel
+    record::String
+    characters::Array{Char, 1}
+    len::Int64
+
+    MyPuzzleRecordModel() = new()
 end
